cc77fede12720fa7da7dbb3f65475f23b8b6c823
Felix Bytow <felix.bytow@googlemail.com>
2014-07-15 16:34:34 +0200

    finished the game

---
+++ main.cxx
@@ -0,0 +1,173 @@
+#include <algorithm>
+#include <random>
+#include <vector>
+#include <iterator>
+#include <iostream>
+#include <fstream>
+#include <chrono>
+#include <string>
+#include <stdexcept>
+
+#include <cstdlib>
+#include <csignal>
+#include <cinttypes>
+
+#include <curses.h>
+
+#include <CL/cl.hpp>
+
+enum class FieldState
+    : std::uint8_t
+{
+    Dead = 0,
+    Alive = 1,
+};
+
+template <template <typename...> class Container>
+void init_field(Container<FieldState> & field)
+{
+    std::random_device rng;
+    std::mt19937 engine(rng());
+    std::uniform_int_distribution<> dist(0, 23);
+
+    std::generate(std::begin(field), std::end(field), [&]() -> FieldState {
+        return dist(engine) ? FieldState::Dead : FieldState::Alive;
+    });
+}
+
+std::string load_kernel(std::string const & filename)
+{
+    std::ifstream source(filename);
+    if (!source.is_open())
+    {
+        source.open("../" + filename);
+        if (!source.is_open())
+            return std::string();
+    }
+
+    return std::string(
+        std::istreambuf_iterator<char>(source),
+        std::istreambuf_iterator<char>()
+    );
+}
+
+template <template <typename...> class Container>
+void render_field(Container<FieldState> const & field)
+{
+    clear();
+
+    for (auto & fs : field)
+    {
+        if (fs == FieldState::Alive)
+            addch(ACS_BLOCK);
+        else
+            addch(' ');
+    }
+
+    refresh();
+}
+
+volatile bool run = true;
+
+void sigint_handler(int)
+{
+    run = false;
+}
+
+int main()
+{
+    try
+    {
+        //////////////
+        // UI setup //
+        //////////////
+        std::signal(SIGINT, &sigint_handler);
+
+        auto screen = initscr();
+        noecho();
+        curs_set(0);
+
+        unsigned width = getmaxx(screen);
+        unsigned height = getmaxy(screen);
+        std::vector<FieldState> field(width * height);
+        init_field(field);
+
+        auto const timeout = std::chrono::seconds(2);
+
+        //////////////////
+        // OpenCl-Setup //
+        //////////////////
+        std::vector<cl::Platform> platforms;
+        cl::Platform::get(&platforms);
+
+        cl_context_properties cprops[] = {
+            CL_CONTEXT_PLATFORM, reinterpret_cast<cl_context_properties>(platforms[0]()),
+            0,
+        };
+
+        cl::Context ctx(CL_DEVICE_TYPE_ALL, cprops, nullptr, nullptr);
+        cl::Buffer in_buffer(ctx, CL_MEM_READ_ONLY | CL_MEM_USE_HOST_PTR, field.size(), field.data());
+        cl::Buffer out_buffer(ctx, CL_MEM_WRITE_ONLY | CL_MEM_USE_HOST_PTR, field.size(), field.data());
+
+        std::string source(load_kernel("rules.cl"));
+        if (source.empty())
+            throw std::runtime_error("Could not load rules.cl!");
+
+        cl::Program::Sources sources = {
+            { source.c_str(), source.length() + 1 },
+        };
+        cl::Program program(ctx, sources);
+
+        auto devices = ctx.getInfo<CL_CONTEXT_DEVICES>();
+        program.build(devices);
+
+        cl::Kernel kernel(program, "GameOfLife");
+        kernel.setArg(0, in_buffer);
+        kernel.setArg(1, out_buffer);
+        kernel.setArg(2, width);
+        kernel.setArg(3, height);
+
+        cl::CommandQueue queue(ctx, devices[0]);
+
+        ///////////////
+        // main loop //
+        ///////////////
+        std::chrono::system_clock::time_point start;
+        while (run)
+        {
+            if (std::chrono::system_clock::now() - start > timeout)
+            {
+                // update the field
+                cl::Event event;
+                queue.enqueueNDRangeKernel(kernel, cl::NullRange, cl::NDRange(field.size()), cl::NDRange(1, 1), nullptr, &event);
+                event.wait();
+
+                queue.enqueueReadBuffer(out_buffer, CL_TRUE, 0, field.size(), field.data());
+                queue.enqueueCopyBuffer(out_buffer, in_buffer, 0, 0, field.size(), nullptr, &event);
+                event.wait();
+
+                render_field(field);
+
+                start = std::chrono::system_clock::now();
+            }
+        }
+
+        /////////////
+        // cleanup //
+        /////////////
+        endwin();
+        return EXIT_SUCCESS;
+    }
+    catch (cl::Error const & err)
+    {
+        endwin();
+        std::cerr << "OpenCL-Error: " << err.what() << "(" << err.err() << ")" << std::endl;
+        return EXIT_FAILURE;
+    }
+    catch (std::exception const & ex)
+    {
+        endwin();
+        std::cerr << "Error: " << ex.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+}
---
+++ rules.cl
@@ -0,0 +1,40 @@
+char cell(__global char const * field, int x, int y, int width, int height)
+{
+    if (x < 0 || y < 0 || x >= width || y >= height)
+        return 0;
+
+    return field[y * width + x];
+}
+
+__kernel void GameOfLife(__global char const * in, __global char * out, int width, int height)
+{
+    int tid = get_global_id(0);
+    int x = tid % width;
+    int y = tid / width;
+
+    char state = in[tid]; // 0 - dead, 1 - alive
+
+    // count the living neighbours
+    int living_neighbours = (
+        cell(in, x - 1, y - 1, width, height) + cell(in, x, y - 1, width, height) + cell(in, x + 1, y - 1, width, height) +
+        cell(in, x - 1, y, width, height)                                         + cell(in, x + 1, y, width, height) +
+        cell(in, x - 1, y + 1, width, height) + cell(in, x, y + 1, width, height) + cell(in, x + 1, y + 1, width, height)
+    );
+
+    if (state == 1)
+    {
+        // only 2 and 3 is good
+        // otherwise the cell dies from under-/overpopulation
+        if (living_neighbours < 2 || living_neighbours > 3)
+            state = 0;
+    }
+    else
+    {
+        // with 3 living neighbours reproduction happens :-)
+        if (living_neighbours == 3)
+            state = 1;
+    }
+
+    // return the result
+    out[tid] = state;
+}
